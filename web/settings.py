9ca91b5f86b01081599b7ff89f233961fe55bb1a
Salvador Gil <salvador.romero@upaep.edu.mx>
2021-05-08 11:55:54 -0500

    Heroku 1.2

--- web/settings.py
+++ web/settings.py
@@ -53,8 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'web.urls'
 
 TEMPLATES = [
